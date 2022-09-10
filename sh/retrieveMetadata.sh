f94b315a85066d46b64202b854f245c88b6be0e5
Kiki Ariady Putra <aurodeus.gmail.com>
2022-09-10 16:49:45 +0700

    Improve metadata retrieval script

--- sh/retrieveMetadata.sh
+++ sh/retrieveMetadata.sh
@@ -3,8 +3,12 @@ CARDANO_NET="mainnet"
 if test -f ~/cardano/cfg/net.cardano; then
     CARDANO_NET=$(cat ~/cardano/cfg/net.cardano)
 fi
+PID=$BF_PID
+if test -f ~/cardano/cfg/pid.bf; then
+    PID=$(cat ~/cardano/cfg/pid.bf)
+fi
 
 # Retrieve METADATA from Blockfrost
-curl    -H  "project_id:$BF_PID"    \
+curl    -H  "project_id:$PID"    \
     https://cardano-$CARDANO_NET.blockfrost.io/api/v0/metadata/txs/labels/$1    \
     |   jq
